5a33126e016d6eb62606bc64fab9e47ecb069d51
Itamar Savir <itamarsavir@Lahavs-MBP.localdomain>
2025-12-31 16:33:43 +0200

    remove roles/resourcemanager.projectIamAdmin to sa

--- infra/terraform/modules/gke/main.tf
+++ infra/terraform/modules/gke/main.tf
@@ -109,7 +109,6 @@ resource "google_project_iam_member" "gke_node_sa_roles" {
     "roles/monitoring.viewer",
     "roles/stackdriver.resourceMetadata.writer",
     "roles/iam.serviceAccountUser",
-    "roles/resourcemanager.projectIamAdmin"
   ]) : toset([])
 
   project = data.google_client_config.default.project
